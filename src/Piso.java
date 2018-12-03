f3d5a7b55130e774f5c8aad51fd3937b1ece534c
Edarcox <eduardo.cabrera@alumnos.uv.cl>
2018-12-02 23:58:05 -0300

    Create Piso.java

---
+++ src/Piso.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Piso {
+int num_piso;
+ArrayList<Habitacion> piso = new ArrayList<Habitacion>();
+////////////////////////////////////////////////////////////
+public void getHabitacion(String tipo) {
+	Habitacion aux = new Habitacion(tipo); 
+	piso.add(aux);
+}
+public void getNum(int num) {
+	this.num_piso = num;
+}
+public int setNum() {
+	return num_piso;
+}
+}
