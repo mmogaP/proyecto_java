6c60de6af54ad97c73e0c0600a915a324e5b8ebf
Edarcox <eduardo.cabrera@alumnos.uv.cl>
2018-12-04 18:43:51 -0300

    Create Archivo_Leer.java

---
+++ src/Archivo_Leer.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.ArrayList;
+public class Archivo_Leer {
+	ArrayList<String> text = new ArrayList<String>();
+public void leerArchivo(String direccion) {
+	try {
+		BufferedReader bf = new BufferedReader(new FileReader(direccion));
+		String linea;
+		while((linea = bf.readLine()) != null) {
+			text.add(linea);
+		}
+		bf.close();
+	}catch(Exception e) {
+		System.out.println("Wea mala");
+	}
+}
+public String getLinea(int num) {
+	return text.get(num);
+}
+public int getTamaño() {
+	return text.size();
+}
+}
