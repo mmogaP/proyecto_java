af91008af83c76c03ea96b2c9f3856ae24d55f38
Edarcox <eduardo.cabrera@alumnos.uv.cl>
2018-12-04 21:54:01 -0300

    Update Precio.java

--- src/Precio.java
+++ src/Precio.java
@@ -1,11 +1,17 @@
-
+import java.util.ArrayList;
 public class Precio {
 	double precioind;
 	double preciodob;
 	double preciomat;
 	double preciocua;
 	double preciosui;
-	
+	ArrayList<String> formato_serv = new ArrayList<String>();
+	ArrayList<Double> precio_serv=new ArrayList<Double>();
+	ArrayList<String> nombre_serv=new ArrayList<String>();
+	ArrayList<String> formato_menu = new ArrayList<String>();
+	ArrayList<Double> precio_menu=new ArrayList<Double>();
+	ArrayList<String> nombre_menu=new ArrayList<String>();
+
 	
 	//////////////////////////
 	public void setHabitacion(String tipo,double precio) {
@@ -24,5 +30,15 @@ public class Precio {
 		else if(tipo.equals("SUITE")) {
 			this.preciosui=precio;
 		}
+	}
+	public void setServicio(String formato,double precio,String nombre) {
+		formato_serv.add(formato);
+		precio_serv.add(precio);
+		nombre_serv.add(nombre);
+	}
+	public void setMenu(String formato,double precio,String nombre) {
+		formato_menu.add(formato);
+		precio_menu.add(precio);
+		nombre_menu.add(nombre);
 	}
 }
