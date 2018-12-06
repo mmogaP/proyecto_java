04eaf992bfca45100e8fcd6d0cfa9a726e7341e1
mmogaP <38256961+mmogaP@users.noreply.github.com>
2018-12-05 23:16:16 -0300

    wea 7

--- src/ImplementacionHotel.java
+++ src/ImplementacionHotel.java
@@ -201,8 +201,14 @@ public class ImplementacionHotel {
 				
 			}
 			
-		}
-				
+			else if(op.getLinea(i).equals("7")) {
+				String report = op.getLinea(i+1);
+				int reporte = Integer.parseInt(report);
+				for(int y=0;y<reporte;y++) {
+					
+				}
+			}			
+		}				
 	}
 }
 
