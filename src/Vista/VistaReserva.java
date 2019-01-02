d693d94ecc75ec5f41f1ed0941cf90d99174eb82
Edarcox <eduardo.cabrera@alumnos.uv.cl>
2019-01-01 23:46:11 -0300

    asd

--- src/Vista/VistaReserva.java
+++ src/Vista/VistaReserva.java
@@ -3,8 +3,10 @@ import java.awt.*;
 import javax.swing.*;
 
 public class VistaReserva extends JFrame {
-	private JLabel tex,tex1,tex2;
-	JTextField cuadro1;
+	private JLabel tex,tex1,tex2,tex3,tex4;
+	private JTextField cuadro1,cuadro2,cuadro3;
+	private TextArea texa;
+	private JButton botona,botonc;
 	public VistaReserva(){
 		setLayout(null);
 		setTitle("Reservar");
@@ -13,14 +15,41 @@ public class VistaReserva extends JFrame {
 		tex.setBounds(155,5,100,50);
 		tex.setFont(new java.awt.Font("Tahoma", 1, 15));
 		add(tex);
-		tex1= new JLabel("Fecha Ingreso");
-		tex1.setBounds(10,50,100,50);
-		add(tex1);
-		cuadro1 = new JTextField();
-		cuadro1.setBounds(100, 60, 100, 25);
-		add(cuadro1);
+		setIngresoText();
 		setVisible(true);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	private void setIngresoText() {
+		tex1= new JLabel("Fecha Ingreso:");
+		tex1.setBounds(10,50,100,50);
+		add(tex1);
+		cuadro1 = new JTextField();
+		cuadro1.setBounds(110, 60, 100, 25);
+		add(cuadro1);
+		tex2 = new JLabel("Fecha de Salida:");
+		tex2.setBounds(10, 100, 100, 50);
+		add(tex2);
+		cuadro2 = new JTextField();
+		cuadro2.setBounds(110, 110, 100, 25);
+		add(cuadro2);
+		tex3 = new JLabel("Habitacion:");
+		tex3.setBounds(10, 150, 100, 50);
+		add(tex3);
+		cuadro3 = new JTextField();
+		cuadro3.setBounds(110, 160, 100, 25);
+		add(cuadro3);
+		tex4 = new JLabel("Personas  Ingresar:");
+		tex4.setBounds(10, 200, 130, 50);
+		add(tex4);
+		texa = new TextArea();
+		texa.setBounds(150, 215, 200, 200);
+		add(texa);
+		botona = new JButton("Aceptar");
+		botona.setBounds(50, 500, 100, 25);
+		add(botona);
+		botonc = new JButton("Cancelar");
+		botonc.setBounds(200, 500, 100, 25);
+		add(botonc);
 	}
 }
